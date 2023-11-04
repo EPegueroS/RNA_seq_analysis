bec8aab8bded6d5c630817de955ab2fbada749df
esteban <esteban.peguero@gmail.com>
2023-11-04 12:00:32 +0100

    resuming pipeline

--- pipelines/run_rnaseq.bash
+++ pipelines/run_rnaseq.bash
@@ -4,4 +4,6 @@ nextflow run nf-core/rnaseq -revision 3.12.0 \
 	-profile docker \
 	--gtf $PWD/genome_references/Homo_sapiens.GRCh38.110.gtf.gz \
 	--fasta $PWD/genome_references/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz \
-	--max_memory 50.GB
+	--max_memory 50.GB \
+	--trimmer fastp \
+	-resume
